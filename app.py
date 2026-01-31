1bafcc35caf21d2b6c2b266680811730b11f88ae
gakusei <t125077.ed.sus.ac.jp>
2026-02-01 04:31:33 +0900

    サイドバー

--- app.py
+++ app.py
@@ -3,4 +3,12 @@ import pandas as pd
 import plotly.express as px
 
 st.title('男女別年齢別人口')
+st.write('年代ごとの男女別年齢別の人口を表示します。')
 df = pd.read_csv('人口ピラミッド_全国（日本）_1920年.csv')
+with st.sidebar:
+    branch = st.multiselect('年代を選択してください（複数選択可）',
+                                df['時点'].unique())
+    year = st.multiselect('年齢を指定してください', 
+                            df['年齢区分'].unique())
+    option = st.radio('表示形式を選択してください',
+                  ['表', 'グラフ'])
