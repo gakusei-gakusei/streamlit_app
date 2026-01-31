51de0f6734f1abf4c2a2bb267c40a411c8ab722e
gakusei <t125077.ed.sus.ac.jp>
2026-02-01 05:34:00 +0900

    表とグラフ１

--- app.py
+++ app.py
@@ -7,8 +7,16 @@ st.write('年代ごとの男女別年齢別の人口を表示します。')
 df = pd.read_csv('人口ピラミッド_全国（日本）_1920年.csv')
 with st.sidebar:
     branch = st.multiselect('年代を選択してください（複数選択可）',
-                                df['時点'].unique())
-    year = st.multiselect('年齢を指定してください', 
-                            df['年齢区分'].unique())
+                                df['年代'].unique())
+    year = st.multiselect('男女を指定してください', 
+                            df['男女'].unique())
     option = st.radio('表示形式を選択してください',
                   ['表', 'グラフ'])
+df = df[df['年代'].isin(branch)]
+df = df[df['男女'].isin(year)]
+if option == '表':
+    st.dataframe(df, width=800, height=220)
+    st.badge("Success", icon=":material/check:", color="green")
+elif option == 'グラフ':
+    st.line_chart(df)
+    st.badge("Success", icon=":material/check:", color="green")
