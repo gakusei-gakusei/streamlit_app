7276a9cf084ee8d53997f3cb18cdeffefc9c2e2e
gakusei <t125077.ed.sus.ac.jp>
2026-02-01 05:46:17 +0900

    サブヘッダー

--- app.py
+++ app.py
@@ -2,21 +2,21 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.title('男女別年齢別人口')
+st.subheader("男女別年齢別人口", divider="gray")
 st.write('年代ごとの男女別年齢別の人口を表示します。')
 df = pd.read_csv('人口ピラミッド_全国（日本）_1920年.csv')
 with st.sidebar:
     branch = st.multiselect('年代を選択してください（複数選択可）',
                                 df['年代'].unique())
-    year = st.multiselect('男女を指定してください', 
+    sex = st.multiselect('男女を指定してください', 
                             df['男女'].unique())
     option = st.radio('表示形式を選択してください',
                   ['表', 'グラフ'])
 df = df[df['年代'].isin(branch)]
-df = df[df['男女'].isin(year)]
+df = df[df['男女'].isin(sex)]
 if option == '表':
     st.dataframe(df, width=800, height=220)
     st.badge("Success", icon=":material/check:", color="green")
 elif option == 'グラフ':
-    st.line_chart(df)
+    st.line_chart(df.T, width=800, height=450)
     st.badge("Success", icon=":material/check:", color="green")
